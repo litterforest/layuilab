3dce5c3cdca348ac9ff197c80d9445a3802b5760
cobee <18027041863@163.com>
2018-05-30 12:01:31 +0800

    提交代码

---
+++ src/main/java/cn/com/cobee/layuilab/web/LayerController.java
@@ -0,0 +1,17 @@
+package cn.com.cobee.layuilab.web;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping(value = "/layer")
+public class LayerController {
+
+    @GetMapping(value = "/showCase")
+    public String showCase()
+    {
+        return "layer/showCase";
+    }
+
+}
---
+++ src/main/webapp/WEB-INF/views/common/header.jsp
@@ -0,0 +1,10 @@
+<%--
+  Created by IntelliJ IDEA.
+  User: Administrator
+  Date: 2018/5/30
+  Time: 11:40
+  To change this template use File | Settings | File Templates.
+--%>
+<%@ page contentType="text/html;charset=UTF-8" language="java" pageEncoding="UTF-8" %>
+<%@ taglib prefix="c" uri="http://java.sun.com/jsp/jstl/core"%>
+<c:set var="ctx" value="${pageContext.request.contextPath}" ></c:set>
